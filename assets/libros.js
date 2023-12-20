40d1e627c274b7cf7c6fc13bfbdbaac770c188f0
Sebas <64394400+sebaslopezg@users.noreply.github.com>
2023-12-20 12:50:23 -0500

    primer commit

---
+++ assets/libros.js
@@ -0,0 +1,210 @@
+const libros = [
+    'Genesis', 
+    'exodo', 
+    'Levitico', 
+    'Numeros', 
+    'Deuteronomio', 
+    'Josue', 
+    'Jueces', 
+    'Rut',
+    '1 Samuel', 
+    '2 Samuel',
+    '1 Reyes', 
+    '2 Reyes', 
+    '1 Cronicas',
+    '2 Cronicas',
+    'Esdras',
+    'Nehemias',
+    'Ester',
+    'Job',
+    'Salmos',
+    'Proverbios',
+    'Eclesiastes',
+    'Cantar de los Cantares',
+    'Isaias',
+    'Jeremias',
+    'Lamentaciones',
+    'Ezequiel',
+    'Daniel',
+    'Oseas',
+    'Joel',
+    'Amos',
+    'Abdias',
+    'Jonas',
+    'Miqueas',
+    'Nahum',
+    'Habacuc',
+    'Sofonías',
+    'Hageo',
+    'Zacarías',
+    'Malaquías',
+    'Mateo',
+    'Marcos',
+    'Lucas',
+    'Juan',
+    'Hechos',
+    'Romanos',
+    '1 Corintios',
+    '2 Corintios',
+    'Galatas',
+    'Efesios',
+    'Filipenses',
+    'Colosenses',
+    '1 Tesalonicenses',
+    '2 Tesalonicenses',
+    '1 Timoteo',
+    '2 Timoteo',
+    'Tito',
+    'Filemon',
+    'Hebreos',
+    'Santiago',
+    '1 Pedro',
+    '2 Pedro',
+    '1 Juan',
+    '2 Juan',
+    '3 Juan',
+    'Judas',
+    'Apocalipsis'
+];
+
+//funcion buscar libro
+function buscarLibro(nombre){
+    let index = libros.indexOf(nombre);
+    switch (index) {
+        case 0:
+            return genesis;
+        case 1:
+            return exodo;
+        case 2:
+            return levitico;
+        case 3:
+            return numeros;
+        case 4:
+            return deuteronomio;
+        case 5:
+            return josue;
+        case 6:
+            return jueces;
+        case 7:
+            return rut;
+        case 8:
+            return samuel_1;
+        case 9:
+            return samuel_2;
+        case 10:
+            return reyes_1;
+        case 11:
+            return reyes_2;
+        case 12:
+            return cronicas_1;
+        case 13:
+            return cronicas_2;
+        case 14:
+            return esdras;
+        case 15:
+            return nehemias;
+        case 16:
+            return ester;
+        case 17:
+            return job;
+        case 18:
+            return salmos;
+        case 19:
+            return proverbios;
+        case 20:
+            return eclesiastes;
+        case 21:
+            return cantares;
+        case 22:
+            return isaias;
+        case 23:
+            return jeremias;
+        case 24:
+            return lamentaciones;
+        case 25:
+            return ezequiel;
+        case 26:
+            return daniel;
+        case 27:
+            return oseas;
+        case 28:
+            return joel;
+        case 29:
+            return amos;
+        case 30:
+            return abdias;
+        case 31:
+            return jonas;
+        case 32:
+            return miqueas;
+        case 33:
+            return nahum;
+        case 34:
+            return habacuc;
+        case 35:
+            return sofonias;
+        case 36:
+            return hageo;
+        case 37:
+            return zacarias;
+        case 38:
+            return malaquias;
+        case 39:
+            return mateo;
+        case 40:
+            return marcos;
+        case 41:
+            return lucas;
+        case 42:
+            return juan;
+        case 43:
+            return hechos;
+        case 44:
+            return romanos;
+        case 45:
+            return corintios_1;
+        case 46:
+            return corintios_2;
+        case 47:
+            return galatas;
+        case 48:
+            return efesios;
+        case 49:
+            return filipenses;
+        case 50:
+            return colosenses;
+        case 51:
+            return tesalonicenses_1;
+        case 52:
+            return tesalonicenses_2;
+        case 53:
+            return timoteo_1;
+        case 54:
+            return timoteo_2;
+        case 55:
+            return tito;
+        case 56:
+            return filemon;
+        case 57:
+            return hebreos;
+        case 58:
+            return santiago;
+        case 59:
+            return pedro_1;
+        case 60:
+            return pedro_2;
+        case 61:
+            return juan_1;
+        case 62:
+            return juan_2;
+        case 63:
+            return juan_3;
+        case 64:
+            return judas;
+        case 65:
+            return apocalipsis;
+      default:
+        //console.log("sin coincidencias");
+        break;
+    }
+  }
---
+++ assets/script.js
@@ -0,0 +1,223 @@
+
+const mostrar = document.getElementById('mostrar');
+const buscar = document.getElementById('buscar');
+const cuadro_busqueda = document.getElementById('cuadro_busqueda');
+const sugerencias = document.querySelector("#sugerencias");
+
+
+buscar.addEventListener("click", busqueda);
+
+cuadro_busqueda.addEventListener("keydown", (e) =>{
+
+  if(e.key == 'Enter'){
+    busqueda();
+  }
+
+});
+
+
+autocomplete(cuadro_busqueda, libros);
+
+
+// funcion de buscar:
+function busqueda(){
+
+  let texto;
+  let letras = cuadro_busqueda.value;
+  let arrBusqueda = letras.split(' ');
+  let arrData = [];
+  let capver;
+  let libroSelecionado;
+
+  arrBusqueda.forEach((item) => {
+    if(item != ""){
+      arrData.push(item);
+    }
+  });
+
+  let primerElemento = parseInt(arrData[0]);
+
+  if(Number.isInteger(primerElemento)){
+    const libro = arrData[0]+" "+arrData[1];
+    libroSelecionado = buscarLibro(libro);
+    capver = arrData[2];
+    limver = arrData[4];
+  }else{
+    const libro = arrData[0];
+    libroSelecionado = buscarLibro(libro);
+    capver = arrData[1];
+    limver = arrData[3];
+  }
+
+  try {
+    texto = buscarCapver(libroSelecionado, capver, limver);
+
+    if(texto == undefined){
+      const frase = arrData[0] + " " + arrData[1] + " " + arrData[2] + " " + arrData[3];
+      if(frase == "Cantar de los Cantares"){
+        libroSelecionado = buscarLibro(frase);
+        capver = arrData[4];
+        limver = arrData[5];
+        try {
+          texto = buscarCapver(libroSelecionado, capver, limver); 
+        }catch (e){
+          console.log("Error en segundo trycatch: " + e);
+        }        
+      }
+    }
+
+    mostrar.innerHTML = texto;
+  }catch{
+    console.log("catch"); //poner aqui condicional
+  }
+
+}
+
+function buscarCapver(array, capver = undefined, limver = undefined){
+  let respuesta = "";
+  if(capver == undefined && limver == undefined){
+
+    //buscar todo el libro
+    
+    array.forEach((element) => {
+      element.forEach( element => {
+        respuesta +=  `<p>${element}</p>`;
+      });
+    });
+    respuesta = formatear(respuesta);
+    return respuesta;
+  }else if(limver == undefined){
+    //Buscar todo el capver
+
+    const arrCapver = capver.split(':');
+    
+    let capitulo;
+    let versiculo = arrCapver[1];
+
+
+    if(arrCapver.length == 1){
+
+      capitulo = parseInt(arrCapver)-1;
+
+      array[capitulo].forEach(item =>{
+        respuesta += `<p>${item}</p>`;
+      });
+
+    }else if(arrCapver.length == 2){
+
+      capitulo = parseInt(arrCapver[0])-1;
+      versiculo = parseInt(arrCapver[1])-1;
+      respuesta = array[capitulo][versiculo];
+    }
+    respuesta = formatear(respuesta);
+    return respuesta;
+
+  }else{
+    //buscar capver con limite
+    // establecer limite para la busqueda [PENDIENTE]
+  }
+}
+
+function formatear(res){
+  return res.replace(/\/n/gi, "<br>");
+}
+
+
+// FUNCION DE AUTOCOMPLETAR
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
